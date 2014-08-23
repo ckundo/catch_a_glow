c8518456e8c5dd3eca15221229cbabd4f695ffa2
Cameron Cundiff <ckundo@gmail.com>
2014-08-23 13:37:18 -0400

    Move test/ to spec/.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'app/js/**/*.js',
-      'test/**/*Spec.js'
+      'spec/**/*Spec.js'
     ],
 
 
