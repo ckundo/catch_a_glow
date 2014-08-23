b18065d5cb6a29c3218486f75d9bd8d47d01344a
Cameron Cundiff <ckundo@gmail.com>
2014-08-23 14:25:02 -0400

    Update specs to test mole timeout.

--- spec/javascript/moleSpec.js
+++ spec/javascript/moleSpec.js
@@ -11,5 +11,21 @@ describe('mole', function() {
       myMole.click();
       expect(myMole.disabled).toBeTruthy();
     });
+
+    describe('after a delay', function() {
+      beforeEach(function() {
+        jasmine.clock().install();
+      });
+
+      afterEach(function() {
+        jasmine.clock().uninstall();
+      });
+
+      it('reenables itself', function() {
+        myMole.click();
+        jasmine.clock().tick(10001);
+        expect(myMole.disabled).toBeFalsy();
+      });
+    })
   });
 });
